8085602ae81ec216e4bd03d51d05b2c72a497c69
exhyn <exhyn@protonmail.com>
2021-05-11 18:51:04 -0300

    prettier

--- subgraphs/bsc/mappings/helpers.ts
+++ subgraphs/bsc/mappings/helpers.ts
@@ -5,7 +5,7 @@ import {
   Bytes,
   dataSource,
   ethereum
-} from '@graphprotocol/graph-ts'
+} from "@graphprotocol/graph-ts";
 import {
   Pool,
   User,
@@ -14,124 +14,144 @@ import {
   TokenPrice,
   Transaction,
   Balancer
-} from '../../../src/types/schema'
-import { BTokenBytes } from '../../../src/types/templates/Pool/BTokenBytes'
-import { BToken } from '../../../src/types/templates/Pool/BToken'
-import { CRPFactory } from '../../../src/types/Factory/CRPFactory'
-import { ConfigurableRightsPool } from '../../../src/types/Factory/ConfigurableRightsPool'
+} from "../../../src/types/schema";
+import { BTokenBytes } from "../../../src/types/templates/Pool/BTokenBytes";
+import { BToken } from "../../../src/types/templates/Pool/BToken";
+import { CRPFactory } from "../../../src/types/Factory/CRPFactory";
+import { ConfigurableRightsPool } from "../../../src/types/Factory/ConfigurableRightsPool";
 
-export let ZERO_BD = BigDecimal.fromString('0')
+export let ZERO_BD = BigDecimal.fromString("0");
 
-let network = dataSource.network()
+let network = dataSource.network();
 
 // Config for mainnet
-let WNATIVE = '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c'
-let USD = '0xe9e7cea3dedca5984780bafc599bd69add087d56'
-let CRP_FACTORY = '0xad346F5c54De58A1B3404A10C98160d2a8951000'
+let WNATIVE = "0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c";
+let USD = "0xe9e7cea3dedca5984780bafc599bd69add087d56";
+let CRP_FACTORY = "0xad346F5c54De58A1B3404A10C98160d2a8951000";
 
 export function hexToDecimal(hexString: string, decimals: i32): BigDecimal {
-  let bytes = Bytes.fromHexString(hexString).reverse() as Bytes
-  let bi = BigInt.fromUnsignedBytes(bytes)
-  let scale = BigInt.fromI32(10).pow(decimals as u8).toBigDecimal()
-  return bi.divDecimal(scale)
+  let bytes = Bytes.fromHexString(hexString).reverse() as Bytes;
+  let bi = BigInt.fromUnsignedBytes(bytes);
+  let scale = BigInt.fromI32(10)
+    .pow(decimals as u8)
+    .toBigDecimal();
+  return bi.divDecimal(scale);
 }
 
 export function bigIntToDecimal(amount: BigInt, decimals: i32): BigDecimal {
-  let scale = BigInt.fromI32(10).pow(decimals as u8).toBigDecimal()
-  return amount.toBigDecimal().div(scale)
+  let scale = BigInt.fromI32(10)
+    .pow(decimals as u8)
+    .toBigDecimal();
+  return amount.toBigDecimal().div(scale);
 }
 
 export function tokenToDecimal(amount: BigDecimal, decimals: i32): BigDecimal {
-  let scale = BigInt.fromI32(10).pow(decimals as u8).toBigDecimal()
-  return amount.div(scale)
+  let scale = BigInt.fromI32(10)
+    .pow(decimals as u8)
+    .toBigDecimal();
+  return amount.div(scale);
 }
 
-export function createPoolShareEntity(id: string, pool: string, user: string): void {
-  let poolShare = new PoolShare(id)
+export function createPoolShareEntity(
+  id: string,
+  pool: string,
+  user: string
+): void {
+  let poolShare = new PoolShare(id);
 
-  createUserEntity(user)
+  createUserEntity(user);
 
-  poolShare.userAddress = user
-  poolShare.poolId = pool
-  poolShare.balance = ZERO_BD
-  poolShare.save()
+  poolShare.userAddress = user;
+  poolShare.poolId = pool;
+  poolShare.balance = ZERO_BD;
+  poolShare.save();
 }
 
-export function createPoolTokenEntity(id: string, pool: string, address: string): void {
-  let token = BToken.bind(Address.fromString(address))
-  let tokenBytes = BTokenBytes.bind(Address.fromString(address))
-  let symbol = ''
-  let name = ''
-  let decimals = 18
+export function createPoolTokenEntity(
+  id: string,
+  pool: string,
+  address: string
+): void {
+  let token = BToken.bind(Address.fromString(address));
+  let tokenBytes = BTokenBytes.bind(Address.fromString(address));
+  let symbol = "";
+  let name = "";
+  let decimals = 18;
 
-  let symbolCall = token.try_symbol()
-  let nameCall = token.try_name()
-  let decimalCall = token.try_decimals()
+  let symbolCall = token.try_symbol();
+  let nameCall = token.try_name();
+  let decimalCall = token.try_decimals();
 
   if (symbolCall.reverted) {
-    let symbolBytesCall = tokenBytes.try_symbol()
+    let symbolBytesCall = tokenBytes.try_symbol();
     if (!symbolBytesCall.reverted) {
-      symbol = symbolBytesCall.value.toString()
+      symbol = symbolBytesCall.value.toString();
     }
   } else {
-    symbol = symbolCall.value
+    symbol = symbolCall.value;
   }
 
   if (nameCall.reverted) {
-    let nameBytesCall = tokenBytes.try_name()
+    let nameBytesCall = tokenBytes.try_name();
     if (!nameBytesCall.reverted) {
-      name = nameBytesCall.value.toString()
+      name = nameBytesCall.value.toString();
     }
   } else {
-    name = nameCall.value
+    name = nameCall.value;
   }
 
   if (!decimalCall.reverted) {
-    decimals = decimalCall.value
+    decimals = decimalCall.value;
   }
 
-  let poolToken = new PoolToken(id)
-  poolToken.poolId = pool
-  poolToken.address = address
-  poolToken.name = name
-  poolToken.symbol = symbol
-  poolToken.decimals = decimals
-  poolToken.balance = ZERO_BD
-  poolToken.denormWeight = ZERO_BD
-  poolToken.save()
+  let poolToken = new PoolToken(id);
+  poolToken.poolId = pool;
+  poolToken.address = address;
+  poolToken.name = name;
+  poolToken.symbol = symbol;
+  poolToken.decimals = decimals;
+  poolToken.balance = ZERO_BD;
+  poolToken.denormWeight = ZERO_BD;
+  poolToken.save();
 }
 
 export function updatePoolLiquidity(id: string): void {
-  let pool = Pool.load(id)
-  let tokensList: Array<Bytes> = pool.tokensList
+  let pool = Pool.load(id);
+  let tokensList: Array<Bytes> = pool.tokensList;
 
   if (pool.tokensCount.equals(BigInt.fromI32(0))) {
-    pool.liquidity = ZERO_BD
-    pool.save()
-    return
+    pool.liquidity = ZERO_BD;
+    pool.save();
+    return;
   }
 
-  if (!tokensList || pool.tokensCount.lt(BigInt.fromI32(2)) || !pool.publicSwap) return
+  if (!tokensList || pool.tokensCount.lt(BigInt.fromI32(2)) || !pool.publicSwap)
+    return;
 
   // Find pool liquidity
 
-  let hasPrice = false
-  let hasUsdPrice = false
-  let poolLiquidity = ZERO_BD
+  let hasPrice = false;
+  let hasUsdPrice = false;
+  let poolLiquidity = ZERO_BD;
 
   if (tokensList.includes(Address.fromString(USD))) {
-    let usdPoolTokenId = id.concat('-').concat(USD)
-    let usdPoolToken = PoolToken.load(usdPoolTokenId)
-    poolLiquidity = usdPoolToken.balance.div(usdPoolToken.denormWeight).times(pool.totalWeight)
-    hasPrice = true
-    hasUsdPrice = true
+    let usdPoolTokenId = id.concat("-").concat(USD);
+    let usdPoolToken = PoolToken.load(usdPoolTokenId);
+    poolLiquidity = usdPoolToken.balance
+      .div(usdPoolToken.denormWeight)
+      .times(pool.totalWeight);
+    hasPrice = true;
+    hasUsdPrice = true;
   } else if (tokensList.includes(Address.fromString(WNATIVE))) {
-    let wnativeTokenPrice = TokenPrice.load(WNATIVE)
+    let wnativeTokenPrice = TokenPrice.load(WNATIVE);
     if (wnativeTokenPrice !== null) {
-      let poolTokenId = id.concat('-').concat(WNATIVE)
-      let poolToken = PoolToken.load(poolTokenId)
-      poolLiquidity = wnativeTokenPrice.price.times(poolToken.balance).div(poolToken.denormWeight).times(pool.totalWeight)
-      hasPrice = true
+      let poolTokenId = id.concat("-").concat(WNATIVE);
+      let poolToken = PoolToken.load(poolTokenId);
+      poolLiquidity = wnativeTokenPrice.price
+        .times(poolToken.balance)
+        .div(poolToken.denormWeight)
+        .times(pool.totalWeight);
+      hasPrice = true;
     }
   }
 
@@ -139,150 +159,175 @@ export function updatePoolLiquidity(id: string): void {
 
   if (hasPrice) {
     for (let i: i32 = 0; i < tokensList.length; i++) {
-      let tokenPriceId = tokensList[i].toHexString()
-      let tokenPrice = TokenPrice.load(tokenPriceId)
+      let tokenPriceId = tokensList[i].toHexString();
+      let tokenPrice = TokenPrice.load(tokenPriceId);
       if (tokenPrice == null) {
-        tokenPrice = new TokenPrice(tokenPriceId)
-        tokenPrice.poolTokenId = ''
-        tokenPrice.poolLiquidity = ZERO_BD
+        tokenPrice = new TokenPrice(tokenPriceId);
+        tokenPrice.poolTokenId = "";
+        tokenPrice.poolLiquidity = ZERO_BD;
       }
 
-      let poolTokenId = id.concat('-').concat(tokenPriceId)
-      let poolToken = PoolToken.load(poolTokenId)
+      let poolTokenId = id.concat("-").concat(tokenPriceId);
+      let poolToken = PoolToken.load(poolTokenId);
 
       if (
-        pool.active && !pool.crp && pool.tokensCount.notEqual(BigInt.fromI32(0)) && pool.publicSwap &&
-        (tokenPrice.poolTokenId == poolTokenId || poolLiquidity.gt(tokenPrice.poolLiquidity)) &&
-        (
-          (tokenPriceId != WNATIVE.toString()) ||
-          (pool.tokensCount.equals(BigInt.fromI32(2)) && hasUsdPrice)
-        )
+        pool.active &&
+        !pool.crp &&
+        pool.tokensCount.notEqual(BigInt.fromI32(0)) &&
+        pool.publicSwap &&
+        (tokenPrice.poolTokenId == poolTokenId ||
+          poolLiquidity.gt(tokenPrice.poolLiquidity)) &&
+        (tokenPriceId != WNATIVE.toString() ||
+          (pool.tokensCount.equals(BigInt.fromI32(2)) && hasUsdPrice))
       ) {
-        tokenPrice.price = ZERO_BD
+        tokenPrice.price = ZERO_BD;
 
         if (poolToken.balance.gt(ZERO_BD)) {
-          tokenPrice.price = poolLiquidity.div(pool.totalWeight).times(poolToken.denormWeight).div(poolToken.balance)
+          tokenPrice.price = poolLiquidity
+            .div(pool.totalWeight)
+            .times(poolToken.denormWeight)
+            .div(poolToken.balance);
         }
 
-        tokenPrice.symbol = poolToken.symbol
-        tokenPrice.name = poolToken.name
-        tokenPrice.decimals = poolToken.decimals
-        tokenPrice.poolLiquidity = poolLiquidity
-        tokenPrice.poolTokenId = poolTokenId
-        tokenPrice.save()
+        tokenPrice.symbol = poolToken.symbol;
+        tokenPrice.name = poolToken.name;
+        tokenPrice.decimals = poolToken.decimals;
+        tokenPrice.poolLiquidity = poolLiquidity;
+        tokenPrice.poolTokenId = poolTokenId;
+        tokenPrice.save();
       }
     }
   }
 
   // Update pool liquidity
 
-  let liquidity = ZERO_BD
-  let denormWeight = ZERO_BD
+  let liquidity = ZERO_BD;
+  let denormWeight = ZERO_BD;
 
   for (let i: i32 = 0; i < tokensList.length; i++) {
-    let tokenPriceId = tokensList[i].toHexString()
-    let tokenPrice = TokenPrice.load(tokenPriceId)
+    let tokenPriceId = tokensList[i].toHexString();
+    let tokenPrice = TokenPrice.load(tokenPriceId);
     if (tokenPrice !== null) {
-      let poolTokenId = id.concat('-').concat(tokenPriceId)
-      let poolToken = PoolToken.load(poolTokenId)
-      if (tokenPrice.price.gt(ZERO_BD) && poolToken.denormWeight.gt(denormWeight)) {
-        denormWeight = poolToken.denormWeight
-        liquidity = tokenPrice.price.times(poolToken.balance).div(poolToken.denormWeight).times(pool.totalWeight)
+      let poolTokenId = id.concat("-").concat(tokenPriceId);
+      let poolToken = PoolToken.load(poolTokenId);
+      if (
+        tokenPrice.price.gt(ZERO_BD) &&
+        poolToken.denormWeight.gt(denormWeight)
+      ) {
+        denormWeight = poolToken.denormWeight;
+        liquidity = tokenPrice.price
+          .times(poolToken.balance)
+          .div(poolToken.denormWeight)
+          .times(pool.totalWeight);
       }
     }
   }
 
-  let factory = Balancer.load('1')
-  factory.totalLiquidity = factory.totalLiquidity.minus(pool.liquidity).plus(liquidity)
-  factory.save()
+  let factory = Balancer.load("1");
+  factory.totalLiquidity = factory.totalLiquidity
+    .minus(pool.liquidity)
+    .plus(liquidity);
+  factory.save();
 
-  pool.liquidity = liquidity
-  pool.save()
+  pool.liquidity = liquidity;
+  pool.save();
 }
 
-export function decrPoolCount(active: boolean, finalized: boolean, crp: boolean): void {
+export function decrPoolCount(
+  active: boolean,
+  finalized: boolean,
+  crp: boolean
+): void {
   if (active) {
-    let factory = Balancer.load('1')
-    factory.poolCount = factory.poolCount - 1
-    if (finalized) factory.finalizedPoolCount = factory.finalizedPoolCount - 1
-    if (crp) factory.crpCount = factory.crpCount - 1
-    factory.save()
+    let factory = Balancer.load("1");
+    factory.poolCount = factory.poolCount - 1;
+    if (finalized) factory.finalizedPoolCount = factory.finalizedPoolCount - 1;
+    if (crp) factory.crpCount = factory.crpCount - 1;
+    factory.save();
   }
 }
 
-export function saveTransaction(event: ethereum.Event, eventName: string): void {
-  let tx = event.transaction.hash.toHexString().concat('-').concat(event.logIndex.toString())
-  let userAddress = event.transaction.from.toHex()
-  let transaction = Transaction.load(tx)
+export function saveTransaction(
+  event: ethereum.Event,
+  eventName: string
+): void {
+  let tx = event.transaction.hash
+    .toHexString()
+    .concat("-")
+    .concat(event.logIndex.toString());
+  let userAddress = event.transaction.from.toHex();
+  let transaction = Transaction.load(tx);
   if (transaction == null) {
-    transaction = new Transaction(tx)
+    transaction = new Transaction(tx);
   }
-  transaction.event = eventName
-  transaction.poolAddress = event.address.toHex()
-  transaction.userAddress = userAddress
-  transaction.gasUsed = event.transaction.gasUsed.toBigDecimal()
-  transaction.gasPrice = event.transaction.gasPrice.toBigDecimal()
-  transaction.tx = event.transaction.hash
-  transaction.timestamp = event.block.timestamp.toI32()
-  transaction.block = event.block.number.toI32()
-  transaction.save()
+  transaction.event = eventName;
+  transaction.poolAddress = event.address.toHex();
+  transaction.userAddress = userAddress;
+  transaction.gasUsed = event.transaction.gasUsed.toBigDecimal();
+  transaction.gasPrice = event.transaction.gasPrice.toBigDecimal();
+  transaction.tx = event.transaction.hash;
+  transaction.timestamp = event.block.timestamp.toI32();
+  transaction.block = event.block.number.toI32();
+  transaction.save();
 
-  createUserEntity(userAddress)
+  createUserEntity(userAddress);
 }
 
 export function createUserEntity(address: string): void {
   if (User.load(address) == null) {
-    let user = new User(address)
-    user.save()
+    let user = new User(address);
+    user.save();
   }
 }
 
 export function isCrp(address: Address): boolean {
-  let crpFactory = CRPFactory.bind(Address.fromString(CRP_FACTORY))
-  let isCrp = crpFactory.try_isCrp(address)
-  if (isCrp.reverted) return false
-  return isCrp.value
+  let crpFactory = CRPFactory.bind(Address.fromString(CRP_FACTORY));
+  let isCrp = crpFactory.try_isCrp(address);
+  if (isCrp.reverted) return false;
+  return isCrp.value;
 }
 
-export function getCrpUnderlyingPool(crp: ConfigurableRightsPool): string | null {
-  let bPool = crp.try_bPool()
+export function getCrpUnderlyingPool(
+  crp: ConfigurableRightsPool
+): string | null {
+  let bPool = crp.try_bPool();
   if (bPool.reverted) return null;
-  return bPool.value.toHexString()
+  return bPool.value.toHexString();
 }
 
 export function getCrpController(crp: ConfigurableRightsPool): string | null {
-  let controller = crp.try_getController()
+  let controller = crp.try_getController();
   if (controller.reverted) return null;
-  return controller.value.toHexString()
+  return controller.value.toHexString();
 }
 
 export function getCrpSymbol(crp: ConfigurableRightsPool): string {
-  let symbol = crp.try_symbol()
-  if (symbol.reverted) return ''
-  return symbol.value
+  let symbol = crp.try_symbol();
+  if (symbol.reverted) return "";
+  return symbol.value;
 }
 
 export function getCrpName(crp: ConfigurableRightsPool): string {
-  let name = crp.try_name()
-  if (name.reverted) return ''
-  return name.value
+  let name = crp.try_name();
+  if (name.reverted) return "";
+  return name.value;
 }
 
 export function getCrpCap(crp: ConfigurableRightsPool): BigInt {
-  let cap = crp.try_getCap()
-  if (cap.reverted) return BigInt.fromI32(0)
-  return cap.value
+  let cap = crp.try_getCap();
+  if (cap.reverted) return BigInt.fromI32(0);
+  return cap.value;
 }
 
 export function getCrpRights(crp: ConfigurableRightsPool): string[] {
-  let rights = crp.try_rights()
-  if (rights.reverted) return []
-  let rightsArr: string[] = []
-  if (rights.value.value0) rightsArr.push('canPauseSwapping')
-  if (rights.value.value1) rightsArr.push('canChangeSwapFee')
-  if (rights.value.value2) rightsArr.push('canChangeWeights')
-  if (rights.value.value3) rightsArr.push('canAddRemoveTokens')
-  if (rights.value.value4) rightsArr.push('canWhitelistLPs')
-  if (rights.value.value5) rightsArr.push('canChangeCap')
-  return rightsArr
+  let rights = crp.try_rights();
+  if (rights.reverted) return [];
+  let rightsArr: string[] = [];
+  if (rights.value.value0) rightsArr.push("canPauseSwapping");
+  if (rights.value.value1) rightsArr.push("canChangeSwapFee");
+  if (rights.value.value2) rightsArr.push("canChangeWeights");
+  if (rights.value.value3) rightsArr.push("canAddRemoveTokens");
+  if (rights.value.value4) rightsArr.push("canWhitelistLPs");
+  if (rights.value.value5) rightsArr.push("canChangeCap");
+  return rightsArr;
 }
